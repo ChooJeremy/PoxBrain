279278e3c50213b842e328581eec076d1779b926
Jeremy Choo <ChooJeremy@Ymail.com>
2017-02-21 06:41:29 +0000

    Updated front page

--- index.php
+++ index.php
@@ -56,27 +56,32 @@
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-2">
+          <!-- 
           <h2>Heading</h2>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          -->
         </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-8">
+          <h2>A new database?</h2>
+          <p>This site is currently available for testing. Please report any bugs, inconsistencies or any comments onto the forum thread.</p>
+          <p>Try out the search function and the rune viewing option! (Start by searching any term on the top left)</p>
+          <p><a class="btn btn-default" href="#" role="button">View thread</a></p>
        </div>
-        <div class="col-md-4">
+        <div class="col-md-2">
+          <!--
           <h2>Heading</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          -->
         </div>
       </div>
 
       <hr>
 
       <footer>
-        <p>&copy; 2017 Company, Inc.</p>
+        <p>&copy; 2017</p>
       </footer>
     </div> <!-- /container -->
     <?php require_once('./js/corejs.php'); ?>
