220d994bbc26ac0e5852a58ac93f69c93d268bcf
Jeremy Choo <ChooJeremy@Ymail.com>
2017-02-21 03:30:48 +0000

    Sub mechanics, conditions and abilties now display help. Some stuff are still not working (i.e. radiant blows) but otherwise will work. Only on web sql for now

--- getnames.php
+++ getnames.php
@@ -31,18 +31,18 @@ while($row = $result->fetch_assoc()) {
     array_push($champions, array( "ID"=> $row["ID"], "Name" =>$row["Name"], "Type"=> 4, "SubText"=> "Equipment", "Explanation"=>"" ) );
 }
 
-$query = "SELECT ID, Name, Description FROM Conditions;";
+$query = "SELECT ID, Identifier, Name, Description FROM Conditions;";
 $result = $mysqli->query($query);
 
 while($row = $result->fetch_assoc()) {
-    array_push($champions, array( "ID"=> $row["ID"], "Name" =>$row["Name"], "Type"=> 5, "SubText"=> "Condition", "Explanation"=> $row["Description"] ) );
+    array_push($champions, array( "ID"=> $row["Identifier"], "Name" =>$row["Name"], "Type"=> 5, "SubText"=> "Condition", "Explanation"=> $row["Description"] ) );
 }
 
-$query = "SELECT ID, Name, Description FROM Mechanics;";
+$query = "SELECT ID, Identifier, Name, Description FROM Mechanics;";
 $result = $mysqli->query($query);
 
 while($row = $result->fetch_assoc()) {
-    array_push($champions, array( "ID"=> $row["ID"], "Name" =>$row["Name"], "Type"=> 6, "SubText"=> "Mechanic", "Explanation"=> $row["Description"] ) );
+    array_push($champions, array( "ID"=> $row["Identifier"], "Name" =>$row["Name"], "Type"=> 6, "SubText"=> "Mechanic", "Explanation"=> $row["Description"] ) );
 }
 
 $query = "SELECT ID, Name, Description, Level FROM Ability;";
--- js/abilitycheck.js
+++ js/abilitycheck.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
                 handleAbility(e, array[0]);
             });
         } else {
-            runQuery("SELECT * FROM Searches WHERE Name = ?", [e.target.textContent.replace("(Default)", "").trim()], function(tx, results) {
+            runQuery("SELECT * FROM Searches WHERE Name = ? AND TYPE = 7", [e.target.textContent.replace("(Default)", "").trim()], function(tx, results) {
                 handleAbility(e, results.rows[0]);
             });
         }
     });
     $(".ability").mouseleave(function(e) {
         $("#ability-popup").css("display", "none");
+        $(".description-popup").remove();
     });
 });
-function handleAbility(e, ability) {
-    var description = ability.Explanation;
+
+function extractAllData(description) {
     //Remove any tags (like <ability value=""></ability>) in the explanation. Or <condition value=""></condition>
     //Sample:
     // <ability value=6>Mobility</ability>
@@ -24,8 +25,7 @@ function handleAbility(e, ability) {
     //var abilityR = new RegExp("(<ability value=(\\d+?)>).+?(</ability>)");
     var conditionR = /(<condition value=([a-z]+?)>).+?(<\/condition>)/g;
     var mechanicR = /(<mechanic value=([a-z_]+?)>).+?(<\/mechanic>)/g;
-    var extraMechanics = [], extraConditions = [], extraAbility = [];
-    var extraToDisplay = [];
+    var results = [];
     //var conditionR = new RegExp("(<condition value=([a-z]+?)>).+?(</condition>)");
     var finalDescription = description;
     do {
@@ -35,7 +35,10 @@ function handleAbility(e, ability) {
             finalDescription = finalDescription.replace(abilityResult[1], "");
             finalDescription = finalDescription.replace(abilityResult[3], "");
             //Step 2: allocate the mechanic to appear by the side
-            extraAbility.push(abilityResult[2]);
+            results.push({
+                "ID": abilityResult[2],
+                "Type": 7
+            });
         }
     } while (abilityResult != null);
     
@@ -46,7 +49,10 @@ function handleAbility(e, ability) {
             finalDescription = finalDescription.replace(conditionResult[1], "");
             finalDescription = finalDescription.replace(conditionResult[3], "");
             //Step 2: allocate the condition to appear by the side
-            extraConditions.push(conditionResult[2]);
+            results.push({
+                "ID": conditionResult[2],
+                "Type": 5
+            });
         }
     } while(conditionResult != null);
     
@@ -57,51 +63,106 @@ function handleAbility(e, ability) {
             finalDescription = finalDescription.replace(mechanicResult[1], "");
             finalDescription = finalDescription.replace(mechanicResult[3], "");
             //Step 2: allocate the condition to appear by the side
-            extraMechanics.push(mechanicResult[2]);
+            results.push({
+                "ID": mechanicResult[2],
+                "Type": 6
+            });
         }
     } while(mechanicResult != null);
     
-    //get them all
-    var firstTime = true;
-    var queryString = "";
-    for(var i = 0; i < extraAbility.length; i++) {
-        if(!firstTime) {
-            queryString = queryString = " UNION ";
-        }
-        queryString = queryString + "SELECT Explanation FROM Searches WHERE Name = " + extraAbility[i] + " AND ID = 7";
-        firstTime = false;
+    return {
+        "Result": results,
+        "Description": finalDescription
+        };
+}
+
+function generatePrompts(promptList, helpLocation, direction) {
+    if(promptList.length == 0) {
+        return;
     }
-    for(i = 0; i < extraConditions.length; i++) {
-        if(!firstTime) {
-            queryString = queryString = " UNION ";
+    if(!window.openDatabase) {
+        //dexie
+    } else {
+        //get them all
+        var firstTime = true;
+        var queryString = "";
+        var params = [];
+        console.log(promptList);
+        for(var i = 0; i < promptList.length; i++) {
+            if(!firstTime) {
+                queryString = queryString + " UNION ";
+            }
+            queryString = queryString + "SELECT Name, Explanation FROM Searches WHERE ID = ? AND Type = " + promptList[i]["Type"];
+            params.push(promptList[i]["ID"]);
+            firstTime = false;
         }
-        queryString = queryString + "SELECT Explanation FROM Searches WHERE Name = " + extraConditions[i] + " AND ID = 5";
-        firstTime = false;
+        
+        console.log(queryString);
+        console.log(params);
+        
+        runQuery(queryString, params, function(tx, results) {
+            
+            console.log("Query ran. Result: ");
+            console.log(results.rows);
+            
+            var resultList = [];
+            var originalY = helpLocation.top;            
+
+            //move up accordingly
+            helpLocation.top = helpLocation.top - (results.rows.length/2.0 * 150);
+            for(var i = 0; i < results.rows.length; i++) {
+                var extractedData = extractAllData(results.rows[i].Explanation);
+                var finalDescription = extractedData["Description"];
+                resultList = resultList.concat(extractedData["Result"]);
+                
+                //make the popup appear, starting from helpLocation
+                var newItem = $(document.createElement("div"));
+                newItem.addClass("description-popup");
+                newItem.css("top", helpLocation.top + "px");
+                newItem.css("left", helpLocation.left + "px");
+                var itemName = $(document.createElement("div"));
+                itemName.addClass("description-header");
+                itemName.text(results.rows[i].Name);
+                var itemDescription = $(document.createElement("div"));
+                itemDescription.text(finalDescription);
+                newItem.append(itemName, itemDescription);
+                console.log(newItem);
+                $("body").append(newItem);
+                
+                helpLocation.top += newItem.height() + 20;
+            }
+            
+            helpLocation.left = helpLocation.left + (275 * direction)
+            helpLocation.top = originalY;
+            
+            generatePrompts(resultList, helpLocation, direction);
+
+        });
     }
-    for(i = 0; i < extraMechanics.length; i++) {
-        if(!firstTime) {
-            queryString = queryString = " UNION ";
-        }
-        queryString = queryString + "SELECT Explanation FROM Searches WHERE Name = " + extraMechanics[i] + " AND ID = 6";
-        firstTime = false;
-    }
-    console.log(queryString);
+}
+
+function handleAbility(e, ability) {
+    var description = ability.Explanation;
+    
+    var extractedData = extractAllData(description);
+    finalDescription = extractedData["Description"];
     
     var position = $(e.target).offset();
-    var helpLocation = position; 
+    var helpLocation = position;
     position.left = position.left + ($(e.target).width()/2);
     $("#ability-popup").text(finalDescription).css("left", position.left).css("display", "block").css("bottom", window.innerHeight - position.top + 20);
-    /*
-    runQuery(queryString, [], function(tx, results) {
-        if(helpLocation.left > window.innerWidth/2) {
-            //Appear on left
-            helpLocation.left = helpLocation.left - 150;
-        } else {
-            //Appear on right
-            helpLocation.left = helpLocation.left + ($(e.target).width()) + 150;
-        }
-        for(var i = 0; i < results.rows.length; i++) {
-            //make the popup appear, starting from helpLocation
-        }
-    });*/
+    
+    var direction = 0;
+    if(helpLocation.left > window.innerWidth / 2) {
+        //Appear on left
+        helpLocation.left = helpLocation.left - 275;
+        direction = -1;
+    } else {
+        //Appear on right
+        helpLocation.left = helpLocation.left + ($(e.target).width()) + 75;
+        direction = 1;
+    }
+    
+    generatePrompts(extractedData["Result"], helpLocation, direction)
+
 }
