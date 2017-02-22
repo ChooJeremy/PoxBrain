dc01ad290e98bab75eed23710c0e99fd71990f3c
Jeremy Choo <ChooJeremy@Ymail.com>
2017-02-22 14:24:58 +0000

    Made the search on the header not hide itself on low res

--- header.php
+++ header.php
@@ -1,15 +1,7 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/">PoxBrain</a>
-    </div>
-    <div id="search-form" class="navbar-collapse collapse">
+    <a class="navbar-brand" href="/">PoxBrain</a>
+    <div id="search-form" class="">
       <form class="navbar-form" action="/search.php" method="get" onsubmit="return performSearch()" >
         <div id="search-group">
           <input type="text" name="search" id="search" placeholder="Search..." autocomplete="off" class="form-control">
