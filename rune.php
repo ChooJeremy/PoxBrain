9a3364c514a1caf8f21c9a9b74beca8256286ab1
Jeremy Choo <ChooJeremy@Ymail.com>
2017-03-16 03:33:21 +0000

    Fixed a bug where the ability: Initate to D'elim search button did not work very well

--- rune.php
+++ rune.php
@@ -234,7 +234,7 @@ if ($auth->isLoggedIn()) {
                   </div>
                 </div>
                 <?php if($item["Type"] == 7) { ?>
-                    <a class="btn btn-primary" style="margin-top: 10px" href='/search.php?search=<?php echo htmlspecialchars("ability:\"".$item["OriginalName"]."\""); ?>'>Find Champions with this ability.</a>
+                    <a class="btn btn-primary" style="margin-top: 10px" href="/search.php?search=<?php echo htmlspecialchars("ability:\"".$item["OriginalName"]."\""); ?>">Find Champions with this ability.</a>
                 <?php } else if($item["Type"] >= 1 && $item["Type"] <= 4) { ?>
                     <p>You own <?php echo $item["Quantity"]; ?> copies of this rune.</p>
                 <?php } ?>
